95c6748fb3abb2c18aacbb2978df0e946dd7eef8
7k7 <montanokevin@outlook.com>
2025-02-12 15:19:06 -0800

    String Assignment
    String Assignment

---
+++ String Assignment/String Assignment/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Concatenate 3 strings //
+
+            // Ask user for first, middle and last name input to concatenate then output
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your middle name?");
+            string middleName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            string fullName = firstName + " " + middleName + " " + lastName;
+            Console.WriteLine($"Your full name is: {fullName}");
+            // Converts a string to uppercase //
+
+            // Lets output that full name in all uppercase letters
+            Console.WriteLine($"Your full name in all caps: {fullName.ToUpper()} ");
+
+            // Creates a Stringbuilder and builds a paragraph text, one sentence at a time. //
+
+            // Loops iterates each time for a sentence a user will input in the console
+            // Stringbuilder var will hold the paragraph of text
+            // isDone is the conditional used to keep the loop iterating until user is done
+            StringBuilder stringBuilder = new StringBuilder();
+            bool isDone = false;
+
+            // Output messgae to user
+            Console.WriteLine("Lets build a paragraph.");
+
+            do
+            {
+                // Adds the sentence given by user to the stringbuilder
+                Console.WriteLine("Ok, please enter one sentence:");
+                stringBuilder.Append(Console.ReadLine());
+
+                // ASk user if done to stop the loop and appending
+                Console.WriteLine("Are you finished with your paragraph? True or False");
+                isDone = Convert.ToBoolean(Console.ReadLine().ToLower());
+            }
+            while (!isDone);
+
+            Console.WriteLine($"Here is your paragraph:\n{stringBuilder}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Escape sequences inside of strings
+            string name = "Jesse";
+            string fileName = "C:\\Users\\Jesse";
+            //can use the @ sign to denote that all is patr of string value
+            // no escape sequescns 
+            string fileNameAtSign = @"C:\Users\Jesse";
+            string quote = "The man said\\, \"Hello World!\", Jesse. \n Hello nw line. \n \t new tab.";
+
+            Console.WriteLine(quote + "\n" + fileName + "\n" + fileNameAtSign);
+
+            // call functions on string var with a string value
+            // these funciotns useful to parse files and look for a character or letter
+            bool trueOrFalse = name.Contains("s");
+            // reassign variable
+            trueOrFalse = name.EndsWith("s");
+            Console.WriteLine(trueOrFalse);
+
+            // sting length
+            int length = name.Length;
+
+            Console.WriteLine(length);
+
+            // Upper caes
+            name = name.ToUpper();
+            Console.WriteLine(name);
+
+            //all lower case
+            name = name.ToLower();
+            Console.WriteLine(name);
+
+            // String vars are immutable
+            // Means once created, string is allocated to memory
+            // once var value is changed, it just generates a new 
+            // string and allocates it to memory
+            // For small programs, it is not that important
+            string userName = "Jesse";
+            userName = "Joe"; // var is ressigned
+
+            Console.WriteLine(userName);
+
+            // It will affect you when changing more that a couple of times
+            // Like continually add more data to the string var over and over
+            // you are not adding but creating a new string each time
+            // program slows down
+            // String builder is a C# class is the solution
+            // It is a dynamic object=it is expandable or shrink wihout performance loss
+            // Example:
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Jesse");
+
+            Console.WriteLine(sb);
+
+            Console.ReadLine();
+        }
+    }
+}
