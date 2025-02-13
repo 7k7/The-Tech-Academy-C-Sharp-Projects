53a8db3427910b2179f6498dbd0aca874c2445e2
7k7 <montanokevin@outlook.com>
2025-02-12 18:01:43 -0800

    Console App Array Submission Assignment
    Console App Array Submission Assignment

---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Arrays 
+        // hold a group of variables
+        // Must define data type the array will hold
+        // integer array called numArray
+        // Must instantiate array 
+        // Must put in the amount of vars it will hold
+        //int[] numArray = new int[5];
+
+        //// Add items to it
+        //// It is zero-based index, meaning it starts with 0 
+        //// and counts it as '1'
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+        //// easier way to put nums in array
+        //// cleaner, easier to read and write
+        //// can add as many items as needed
+        //// once lenfth is set, it cannot be increased.
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 };
+        //Console.WriteLine(numArray1[3]); // shoudl be 200
+
+        //// faster way:
+        //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        //// change value but can't change size of array
+        //numArray2[5] = 650;
+        //// output only one array item
+        //Console.WriteLine(numArray2[3]); // 200
+        //Console.WriteLine(numArray2[5]); // 650
+
+
+
+        // Lists
+        // much more adaptable than arrays
+        // It is called generic because a List can hold any data type like array
+        // It is from System.Collections.Generic library
+        List<int> intList = new List<int>();
+        // Add to List
+        intList.Add(4);
+        intList.Add(10);
+        // Remove a specified value
+        intList.Remove(10); 
+
+        Console.WriteLine(intList[0]);
+
+        // String List
+        List<string> stringList = new List<string>();
+        stringList.Add("Helo");
+        stringList.Add("Kevin");
+        stringList.Remove("Kevin");
+
+        Console.WriteLine(stringList[0]);
+
+        // Use array when you have a fixed quantity to group that won't change
+        // Or when storing a very large quantity 
+        // FOr anything else, ues list. 
+        Console.ReadLine();
+    }
+}
---
+++ Console App Array Sub Assignment/Console App Array Sub Assignment/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Console_App_Array_Sub_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // One-dimensional Array of strings. User selects an index 
+            // to display it 
+
+            // of array of a groceries list will be used
+            string[] groceries = { "banannas", "milk", "cheese", "salad" };
+
+            // Prompt user to input an integer
+            Console.WriteLine("You have four items in your groceries list.");
+            Console.WriteLine("Please type 0, 1, 2, or 3 to view an item.");
+
+            // Output index value based on the user input
+            int userSelectedIndex = Convert.ToInt32(Console.ReadLine());
+
+            // Message will display depending on whether the chosen index exists
+            string printGroceriesMessage = userSelectedIndex >= 0 && userSelectedIndex < groceries.Length
+                ? $"You have selected: {groceries[userSelectedIndex]}"
+                : $"This index, '{userSelectedIndex}', does not exist."; 
+            Console.WriteLine(printGroceriesMessage);
+
+            // One-dimensional Array of strings. User selects an index 
+            // to display it 
+
+            // Array of registers at a grocery store
+            int[] registers = { 2, 5, 7, 1 };
+
+            // Ask user to input an integer 
+            Console.WriteLine("There are four registers open to help you checkout.");
+            Console.WriteLine("Please type either 0, 1, 2, or 3 to select an open register.");
+
+            // Prints to console the index value chosen by the user
+            userSelectedIndex = Convert.ToInt32(Console.ReadLine());
+            string message = userSelectedIndex >= 0 && userSelectedIndex < registers.Length
+                ? $"You may proceed to register: {registers[userSelectedIndex]}"
+                : $"This index, '{userSelectedIndex}'. does not exist.";
+            Console.WriteLine(message);
+
+            // Now the same for a list of strings //
+
+            // A List this time of ways to carry your groceries
+            List<string> options = new List<string>();
+            // Add items
+            options.Add("plastic bag");
+            options.Add("reusable bag");
+            options.Add("your own bag");
+            options.Add("none");
+
+            // Prompt user to input an integer
+            Console.WriteLine("Now choose either 0, 1, 2, or 3 to select one option to carry your groceries:");
+
+            // Store input for later eval
+            userSelectedIndex = Convert.ToInt32(Console.ReadLine());
+
+            // Based on the index chosen by user
+            // One of two messages will print
+            message = userSelectedIndex >= 0 && userSelectedIndex < options.Count
+                ? $"You have selected: {options[userSelectedIndex]}"
+                : $"This index, '{userSelectedIndex}', does not exist.";
+            // print message
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+
+        }
+    }
+}
