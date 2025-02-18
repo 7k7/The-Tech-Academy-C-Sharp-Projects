5d4b5acc85c101545e85017c8d6ed6bd3464873b
7k7 <montanokevin@outlook.com>
2025-02-17 23:02:21 -0800

    METHOD_SUBMISSION_ASSIGNMENT
    METHOD_SUBMISSION_ASSIGNMENT

---
+++ METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/Calculator.cs
@@ -0,0 +1,15 @@
+﻿
+namespace METHOD_SUBMISSION_ASSIGNMENT
+{
+    // This class represents a calculator for performing
+    // mathematical operations on numbers
+    internal class Calculator
+    {
+        // f() takes in two integers, with one optional parameter
+        // using a default value which is used if not argument is passed in
+        public int Add(int num1, int num2 = 1)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace METHOD_SUBMISSION_ASSIGNMENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate Calculator 
+            Calculator calculator = new Calculator();
+
+            // Prompt user for two numbers one at a time
+            // Print a message that the 2nd # is optional
+            Console.WriteLine("Please enter a number: ");
+            int inputNum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please the second number. " +
+                "(Or press Enter to use the default value 1): ");
+            string inputNum2 = Console.ReadLine();
+
+            // Hold the return value of the f() call
+            int result;
+
+            // Check if user provided a second number 
+            if (string.IsNullOrWhiteSpace(inputNum2))
+            {
+                // Only the first number is passed if True
+                result = calculator.Add(inputNum1);
+            }
+            else
+            {
+                // but if there is a second number
+                // pass both to Add()
+                int num2 = Convert.ToInt32(inputNum2);
+                result = calculator.Add(inputNum1, num2);
+            }
+            // print the result 
+            Console.WriteLine("The sum: {0}", result);
+            
+            Console.ReadLine();
+        }
+    }
+}
