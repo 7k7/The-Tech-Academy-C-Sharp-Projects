e4572278b14252aa5f76093a0b55f32b9b10105c
7k7 <montanokevin@outlook.com>
2025-02-17 23:15:26 -0800

    METHOD_CLASS_SUBMISSION_ASSIGNMENT
    METHOD_CLASS_SUBMISSION_ASSIGNMENT

---
+++ METHOD CLASS SUBMISSION ASSIGNMENT/METHOD CLASS SUBMISSION ASSIGNMENT/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHOD_CLASS_SUBMISSION_ASSIGNMENT
+{
+    // This class performs certain math operations
+    internal class Calculator
+    {
+        // This method takes in two integers as parameters.
+        // Adds the first and second then display the second number
+        public void Calculate(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine($"This is the second integer: {num2}");
+        }
+    }
+}
---
+++ METHOD CLASS SUBMISSION ASSIGNMENT/METHOD CLASS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHOD_CLASS_SUBMISSION_ASSIGNMENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instance of Caculator
+            Calculator calc = new Calculator();
+
+            // Call Calculate() and pass two numbers
+            // Specify the parameters by name
+            calc.Calculate(num1: 50, num2: 10);
+        }
+    }
+}
