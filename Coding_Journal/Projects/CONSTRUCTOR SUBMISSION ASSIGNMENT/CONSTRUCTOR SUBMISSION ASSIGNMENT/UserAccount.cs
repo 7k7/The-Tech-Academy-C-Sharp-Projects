5717d4b9746fa60d3f287fddc495dcc642eafb30
7k7 <montanokevin@outlook.com>
2025-02-25 20:51:27 -0800

    CONSTRUCTOR_SUBMISSION_ASSIGNMENT
    CONSTRUCTOR_SUBMISSION_ASSIGNMENT

--- Coding_Journal/Projects/CONSTRUCTOR SUBMISSION ASSIGNMENT/CONSTRUCTOR SUBMISSION ASSIGNMENT/UserAccount.cs
+++ Coding_Journal/Projects/CONSTRUCTOR SUBMISSION ASSIGNMENT/CONSTRUCTOR SUBMISSION ASSIGNMENT/UserAccount.cs
@@ -1,18 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace CONSTRUCTOR_SUBMISSION_ASSIGNMENT
 {
+    // This UserAccount class represents a generic account for a person
+    // Properties are generic
     public class UserAccount
     {
+        // Class constructor methods
+        // This demonstrates chaining methods
+        // Their purpose of each is to provide default parameter values
+        // for any arguments a user does not provide
+        // This ensures that all properties are initialized and assigned
         public UserAccount() : this("User1")
         {
             
         }
-
+       
         public UserAccount(string userName) : this(userName, "none", "none")
         {
 
@@ -22,7 +24,9 @@ namespace CONSTRUCTOR_SUBMISSION_ASSIGNMENT
         {
 
         }
-
+        // This constructor is where each property is
+        // initialized and assigned either a default property
+        // or arguments when this class is instantiated
         public UserAccount(string userName, string firstName, string lastName, int age)
         {
             this.userName = userName;
@@ -31,15 +35,15 @@ namespace CONSTRUCTOR_SUBMISSION_ASSIGNMENT
             this.age = age;
         }
 
-
-
-        // properties
+        // properties set to private as this data is sensitive
+        // and don't want to expose the fields anywhere else
         private string userName;
         private string firstName;
         private string lastName;
         private int age;
 
-        // getters
+        // getters for to retrieve property values only
+        // while protecting the fields
         public string UserName { get { return userName; } }
         public string FirstName { get { return firstName; } }
         public string LastName { get { return lastName; } }
