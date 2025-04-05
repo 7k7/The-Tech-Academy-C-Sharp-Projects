51e7ab23d1ca6d444c0ffbdd31c57710d8129e4a
7k7 <montanokevin@outlook.com>
2025-04-04 17:07:14 -0700

    FinalChallengeSubAssignment
    FinalChallengeSubAssignment

---
+++ Coding_Journal/Projects/StudentDBApp/StudentDBApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDBApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiates a Student context to represent a session operations to
+            // the Student db
+            // in the using statement 
+            using (StudentContext context = new StudentContext())
+            {
+                // Instantiates a Student and populates with data
+                // To later perform CRUD ops
+                Student student = new Student()
+                {
+                    StudentName = "Johnny Smith",
+                    DateOfBirth = DateTime.Now,
+                    Grade = 7
+                };
+                // Adds Student object to the StudentContext current session
+                context.Students.Add(student);
+                // Performs and saves all changes of the current DbContext session
+                // to the Student DB. Essentially adding the Student to the DB
+                context.SaveChanges();
+                // Success output message
+                Console.WriteLine("The Student is saved the the database!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Coding_Journal/Projects/StudentDBApp/StudentDBApp/SchoolContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDBApp
+{
+    // Represents the database context for the Student database
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base() { }
+        // Provides the DbSet of type Student class called Students
+        public DbSet<Student> Students { get; set; }
+    }
+}
